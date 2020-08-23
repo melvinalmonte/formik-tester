2f29555e978069d9ea7c7b40cf837f65ced70398
Melvin Almonte <melvinalmonte@gmail.com>
2020-08-22 23:37:01 -0400

    fixed tiny responsive issue

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export const App = () => {
   const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
   return (
-    <div className="app">
+    <>
       <Particles
         id="tsparticles"
         canvasClassName="particle-canvas"
@@ -161,6 +161,6 @@ export const App = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
